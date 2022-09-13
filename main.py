b62f24402a7d976892602bddcadc7b84d858015a
lorenamucheniski <lomucheniski25@gmail.com>
2022-09-12 23:12:42 -0300

    criando frames e definindo cores

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+#importando tkinter
 from tkinter import *
 from tkinter import ttk
 
 #cores
-cor1 = '#080808'    #preta
+cor1 = '#080808'    #preta/black
+cor2 = '#fafcff'    #branco/white
+cor3 = '#32b2fc'    #azul
+cor4 = '#797f82'    #cizenta
+cor5 = '#f7570c'    #laranja/orange
 
 janela = Tk()
 janela.title('CALCULADORA')
 janela.geometry('270x330')
 janela.config(bg= cor1)
 
-
-frame_tela = Frame(janela , width= 270, height= 50)
+#criando frames
+frame_tela = Frame(janela , width= 270, height= 50, bg= cor3)
 frame_tela.grid(row= 0, column= 0)
+
+frame_corpo = Frame(janela, width= 270, height=280)
+frame_corpo.grid(row= 1, column= 0)
 
 janela.mainloop()
