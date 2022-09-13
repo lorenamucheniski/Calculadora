6496470fa9b606cd7a32d315ac3909c1faf05477
lorenamucheniski <lomucheniski25@gmail.com>
2022-09-13 12:42:08 -0300

    adição dos botões e suas configurações

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 #cores
 cor1 = '#080808'    #preta/black
 cor2 = '#fafcff'    #branco/white
-cor3 = '#32b2fc'    #azul
+cor3 = '#243663'    #azul
 cor4 = '#797f82'    #cizenta
 cor5 = '#f7570c'    #laranja/orange
 
@@ -20,5 +20,13 @@ frame_tela.grid(row= 0, column= 0)
 
 frame_corpo = Frame(janela, width= 270, height=280)
 frame_corpo.grid(row= 1, column= 0)
+
+#criando botões
+b1 = Button(frame_corpo, text = 'C', width= 18, height= 2, bg= cor4, font= 'Ivy 13 bold', relief= RAISED, overrelief= RIDGE)    #botão Clean
+b1.place(x= 0, y= 0)
+b2 = Button(frame_corpo, text= '%', width= 8, height=2, bg= cor4)    #botão do módulo
+b2.place(x= 136, y=0 )
+b3 = Button(frame_corpo, text= '/', width=8, height=2, bg= cor5, fg= cor2)     #botão de divisão
+b3.place(x= 202, y= 0)
 
 janela.mainloop()
